e523d5aa43830428e1cfb96299c62e4d68bcd3e2
donataskartanovicius <donatas.karta@gmail.com>
2022-07-29 21:18:28 +0300

    Test, Storybook, babel added

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-
-// NEW
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from "rollup-plugin-postcss";
 
 const packageJson = require("./package.json");
@@ -24,20 +24,18 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-
-      // NEW
-      postcss(), 
+      postcss(),
+      terser(),
     ],
   },
   {
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-
-    // NEW
     external: [/\.css$/],
   },
 ];
