6b4c8cbb49947a63b24eb9c31860c4f008cf5e96
donataskartanovicius <donatas.karta@gmail.com>
2022-07-31 17:06:33 +0300

    feat: add utilities (theme, units, helpers)

---
+++ src/utils/theme.ts
@@ -0,0 +1,112 @@
+import { spacing } from './units';
+
+const white = '#fff';
+const black = '#000';
+
+const palette = {
+  common: {
+    black,
+    white,
+  },
+  primary: {
+    main: '#04008A',
+    light: '#4A449D',
+    contrastText: '#F8F8F8',
+  },
+  surface: {
+    main: '#BDBDBD',
+    light: '#F2F2F2',
+    hover: '#CCCCCC',
+    contrastText: '#222222'
+  },
+  error: {
+    main: '#b62b28',
+    light: '#fad7d6',
+    contrastText: white,
+  },
+  link: {
+    link: '#0000FF'
+  },
+  grey: {
+    50: '#F7F7F7',
+    100: '#EFEFEF',
+    200: '#DFDFDF',
+    300: '#cacaca',
+    400: '#a8a8a8',
+    500: '#878787',
+    600: '#6d6d6d',
+    700: '#5F5F5F',
+    800: '#4A4A4A',
+    900: '#3D3D3D'
+  },
+  blue: {
+    50: '#eaeef8',
+    100: '#d6ddf1',
+    200: '#acbce4',
+    300: '#839ad6',
+    400: '#5979c9',
+    500: '#3057bb',
+    600: '#264696',
+    700: '#1d3470',
+    800: '#13234b',
+    900: '#0a1125'
+  },
+  red: {
+    50: '#fcebeb ',
+    100: '#fad7d6',
+    200: '#f4afad',
+    300: '#ef8684',
+    400: '#e95e5b',
+    500: '#e43632',
+    600: '#b62b28',
+    700: '#89201e',
+    800: '#5b1614',
+    900: '#2e0b0a'
+  },
+  green: {
+    50: '#eaeef8',
+    100: '#d6ddf1',
+    200: '#acbce4',
+    300: '#839ad6',
+    400: '#5979c9',
+    500: '#3057bb',
+    600: '#264696',
+    700: '#1d3470',
+    800: '#13234b',
+    900: '#0a1125'
+  },
+  orange: {
+    50: '#fef1ea',
+    100: '#fee3d5',
+    200: '#fdc8aa',
+    300: '#fbac80',
+    400: '#fa9155',
+    500: '#f9752b',
+    600: '#c75e22',
+    700: '#95461a',
+    800: '#642f11',
+    900: '#321709'
+  },
+  yellow: {
+    50: '#fffee6',
+    100: '#fffdcc',
+    200: '#fffb99',
+    300: '#fffa67',
+    400: '#fff834',
+    500: '#fff601',
+    600: '#ccc501',
+    700: '#999401',
+    800: '#666200',
+    900: '#333100'
+  },
+};
+
+const typography = {
+  fontFamily:
+    "'Jost', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Ubuntu, 'Helvetica Neue', sans-serif",
+};
+
+export const theme = {
+  palette,
+  typography,
+};
